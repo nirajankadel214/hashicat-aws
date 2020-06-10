b3b87365b123a8e9bdab6929df563aea6aef4824
nirajankadel214 <nirajan_1300@yahoo.com>
2020-06-10 18:30:27 +0000

    added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,10 @@
 module "vpc" {
-  
-  source  = "app.terraform.io/nirajankadel-training/vpc/aws"
-  version = "1.63.0"
-}
+  source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
