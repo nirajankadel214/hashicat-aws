fbdd07a5baeff8370d241d98732f6e51546bbee0
nirajankadel214 <nirajan_1300@yahoo.com>
2020-06-10 18:25:17 +0000

    added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,8 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  
+  source  = "app.terraform.io/nirajankadel-training/vpc/aws"
+  version = "1.63.0"
+}
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
